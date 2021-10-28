96cdf9bc59f770e52dc77972f0b1089b0301eb2a
vladp7ay3r <khalipov.vlad@gmail.com>
2021-10-29 00:12:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click',function(){
+	mobileNavIcon.classList.toggle('active');
+	mobileNav.classList.toggle('active');
+})
+
